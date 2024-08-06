057c2ae7edef5ae637604d3ed63cd739812b1ed2
Vollow <vollowx@proton.me>
2024-08-06 23:56:26 +0800

    fix(copilot): correctly disable default keymap

--- lua/configs/copilot.lua
+++ lua/configs/copilot.lua
@@ -1,4 +1,4 @@
 require('copilot').setup({
-  suggestion = { enabled = true, auto_trigger = true, keymap = { accept = '' } },
+  suggestion = { enabled = true, auto_trigger = true, keymap = { accept = '<Nop>' } }, -- Mapped to <Tab> in core._internals.completion
   panel = { enabled = false },
 })
