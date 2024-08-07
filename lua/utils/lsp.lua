6491c68e4d80d9c1d4d3fff87ca34f09ffbb655a
Vollow <vollowx@proton.me>
2024-08-07 12:34:58 +0800

    feat(lsp): set `hostInfo` to `neovim`

--- lua/utils/lsp.lua
+++ lua/utils/lsp.lua
@@ -2,7 +2,10 @@ local M = {}
 
 ---@type lsp_client_config_t
 ---@diagnostic disable-next-line: missing-fields
-M.default_config = { root_patterns = require('utils.fs').root_patterns }
+M.default_config = {
+  root_patterns = require('utils.fs').root_patterns,
+  init_options = { hostInfo = 'neovim' },
+}
 
 ---@class vim.lsp.ClientConfig: lsp_client_config_t
 ---@class lsp_client_config_t
